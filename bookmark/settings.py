7e7e446c54f9c050ad6d73929e2b9103e55f9f5d
alaniomotosho2 <alaniomotosho2@gmail.com>
2017-10-26 22:58:39 -0400

    added custome authentication

--- bookmark/settings.py
+++ bookmark/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
 }
 
 
+#custom authenticatrion with aunthentication.py file i created
+#by using email as well for authentication
+AUTHENTICATION_BACKENDS = (
+'django.contrib.auth.backends.ModelBackend',
+'account.authentication.EmailAuthBackend',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
